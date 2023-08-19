0862ab80e34eff397cd068a500d4f924b2dce933
Sagar-Karmoker <sagarkarmoker2001@gmail.com>
2023-08-19 15:39:49 +0600

    Mentor RequestPayment added

---
+++ app/src/main/java/com/cse489/tutorbridge/RequestPayment.java
@@ -0,0 +1,14 @@
+package com.cse489.tutorbridge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_payment);
+    }
+}
