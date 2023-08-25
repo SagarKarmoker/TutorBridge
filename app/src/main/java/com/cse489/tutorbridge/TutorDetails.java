327650344c1f90507b7af4c51abb44d2df852df1
WebAdib <142529792+WebAdib@users.noreply.github.com>
2023-08-25 21:47:24 +0600

    Add files via upload

---
+++ app/src/main/java/com/cse489/tutorbridge/TutorDetails.java
@@ -0,0 +1,16 @@
+package com.cse489.tutorbridge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cse489.tutorbridge.R;
+
+public class TutorDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_details);
+    }
+}
