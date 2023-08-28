fe50c7251ee14d374468fa18a009ec98118cadb7
WebAdib <142529792+WebAdib@users.noreply.github.com>
2023-08-29 03:43:05 +0600

    Update initialActivity.java

--- app/src/main/java/com/cse489/tutorbridge/initialActivity.java
+++ app/src/main/java/com/cse489/tutorbridge/initialActivity.java
@@ -2,14 +2,18 @@ package com.cse489.tutorbridge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class initialActivity extends AppCompatActivity {
     private Button initialSignup,initialLogin;
+    private ProgressBar progressBar;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,22 +21,27 @@ public class initialActivity extends AppCompatActivity {
 
         initialSignup = findViewById(R.id.initialSignup);
         initialLogin = findViewById(R.id.initialLogin);
+        progressBar = findViewById(R.id.progressBar);
 
 
         initialSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 //initial page to signup page
                 Intent i1 = new Intent(initialActivity.this, SignupActivity.class);
                 startActivity(i1);
+                progressBar.setVisibility(View.GONE);
             }
         });
         initialLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 //initial page to signup page
                 Intent i2 = new Intent(initialActivity.this, LoginActivity.class);
                 startActivity(i2);
+                progressBar.setVisibility(View.GONE);
             }
         });
     }
