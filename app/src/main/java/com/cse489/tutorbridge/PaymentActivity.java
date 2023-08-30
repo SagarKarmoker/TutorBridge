25ae7764ec966be21a571f103e4365a59e225765
Sagar-Karmoker <sagarkarmoker2001@gmail.com>
2023-08-31 00:23:47 +0600

    payment system test version added

--- app/src/main/java/com/cse489/tutorbridge/PaymentActivity.java
+++ app/src/main/java/com/cse489/tutorbridge/PaymentActivity.java
@@ -1,12 +1,35 @@
 package com.cse489.tutorbridge;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.cse489.tutorbridge.modal.HistoryClass;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PaymentActivity extends AppCompatActivity {
+import com.razorpay.Checkout;
+import com.razorpay.ExternalWalletListener;
+import com.razorpay.PaymentData;
+import com.razorpay.PaymentResultWithDataListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+public class PaymentActivity extends AppCompatActivity implements PaymentResultWithDataListener{
+    private static final String TAG = PaymentActivity.class.getSimpleName();
+    private AlertDialog.Builder alertDialogBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +39,124 @@ public class PaymentActivity extends AppCompatActivity {
         HistoryClass history = (HistoryClass) getIntent().getSerializableExtra("order");
         assert history != null;
         System.out.println(history.toString());
+
+
+        /*
+         To ensure faster loading of the Checkout form,
+          call this method as early as possible in your checkout flow.
+         */
+        Checkout.preload(getApplicationContext());
+
+        // Payment button created by you in XML layout
+        Button button = (Button) findViewById(R.id.btn_pay);
+
+        alertDialogBuilder = new AlertDialog.Builder(PaymentActivity.this);
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setTitle("Payment Result");
+        alertDialogBuilder.setPositiveButton("Ok", (dialog, which) -> {
+            //do nothing
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPayment();
+            }
+        });
+
+        /*TextView privacyPolicy = (TextView) findViewById(R.id.txt_privacy_policy);
+
+        privacyPolicy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent httpIntent = new Intent(Intent.ACTION_VIEW);
+                httpIntent.setData(Uri.parse("https://razorpay.com/sample-application/"));
+                startActivity(httpIntent);
+            }
+        });*/
+
+    }
+
+    public void startPayment() {
+        /*
+          You need to pass current activity in order to let Razorpay create CheckoutActivity
+         */
+        final Activity activity = this;
+
+        final Checkout co = new Checkout();
+
+        //rzp_test_jz5wiS63wA0WlE api
+        EditText etApiKey = findViewById(R.id.et_api_key);
+        etApiKey.setText("rzp_test_jz5wiS63wA0WlE");
+        if (!TextUtils.isEmpty(etApiKey.getText().toString())){
+            co.setKeyID("rzp_test_jz5wiS63wA0WlE");
+        }
+        EditText etCustomOptions = findViewById(R.id.et_custom_options);
+        if (!TextUtils.isEmpty(etCustomOptions.getText().toString())){
+            try{
+                JSONObject options = new JSONObject(etCustomOptions.getText().toString());
+                co.open(activity, options);
+            }catch (JSONException e){
+                Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
+                        .show();
+                e.printStackTrace();
+            }
+        }else{
+            try {
+                JSONObject options = new JSONObject();
+                options.put("name", "TutorBridge Payment");
+                options.put("description", "Demoing Charges");
+                options.put("send_sms_hash",true);
+                options.put("allow_rotation", true);
+                //You can omit the image option to fetch the image from dashboard
+                options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+                options.put("currency", "BDT");
+                options.put("amount", "100");
+
+                JSONObject preFill = new JSONObject();
+                preFill.put("email", "test@razorpay.com");
+                preFill.put("contact", "9876543210");
+
+                options.put("prefill", preFill);
+
+                co.open(activity, options);
+            } catch (Exception e) {
+                Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
+                        .show();
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public void onExternalWalletSelected(String s, PaymentData paymentData) {
+        try{
+            alertDialogBuilder.setMessage("External Wallet Selected:\nPayment Data: "+paymentData.getData());
+            alertDialogBuilder.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void onPaymentSuccess(String s, PaymentData paymentData) {
+        try{
+            alertDialogBuilder.setMessage("Payment Successful :\nPayment ID: "+s+"\nPayment Data: "+paymentData.getData());
+            alertDialogBuilder.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void onPaymentError(int i, String s, PaymentData paymentData) {
+        try{
+            alertDialogBuilder.setMessage("Payment Failed:\nPayment Data: "+paymentData.getData());
+            alertDialogBuilder.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
