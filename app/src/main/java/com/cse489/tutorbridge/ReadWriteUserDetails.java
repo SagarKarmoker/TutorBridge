eb05c56ff034236c7234690f89fb015b735c97e7
WebAdib <142529792+WebAdib@users.noreply.github.com>
2023-09-02 01:35:37 +0600

    Add files via upload

---
+++ app/src/main/java/com/cse489/tutorbridge/ReadWriteUserDetails.java
@@ -0,0 +1,18 @@
+package com.cse489.tutorbridge;
+
+public class ReadWriteUserDetails {
+    public String textName,textPhone,textAddress,textEducation,
+            textExpert,textYears,textPrice,textDescription;
+    public ReadWriteUserDetails(String textName,String textPhone,String textAddress,String textEducation,
+                                String textExpert,String textYears,String textPrice,String textDescription){
+        this.textName = textName;
+        this.textPhone = textPhone;
+        this.textAddress = textAddress;
+        this.textEducation = textEducation;
+        this.textExpert = textExpert;
+        this.textYears = textYears;
+        this.textPrice = textPrice;
+        this.textDescription = textDescription;
+    }
+
+}
