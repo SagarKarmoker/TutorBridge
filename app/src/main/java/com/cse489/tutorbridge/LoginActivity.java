386e9da92ac00626f14e4d18dd283ac9eaecf4f1
WebAdib <142529792+WebAdib@users.noreply.github.com>
2023-09-01 12:18:27 +0600

    Update LoginActivity.java

--- app/src/main/java/com/cse489/tutorbridge/LoginActivity.java
+++ app/src/main/java/com/cse489/tutorbridge/LoginActivity.java
@@ -1,22 +1,34 @@
 package com.cse489.tutorbridge;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ImageView ivBackArrowL;
-    private TextView btnToggleL,toggleText,tvTittle,tvPera1,tvUserName,tvPassword;
-    private EditText etUserName,etPassword;
+    private TextView btnToggleL,toggleText,tvTittle,tvPera1,tvEmail,tvPassword;
+    private EditText etEmail,etPassword;
     private Button btnLoginPage;
     public static final String SHARED_PREFS = "sharedPrefs";
+    private ProgressBar progressBar;
+    private FirebaseAuth authProfile;
 
 
 
@@ -31,11 +43,15 @@ public class LoginActivity extends AppCompatActivity {
         btnToggleL = findViewById(R.id.btnToggleL);
         tvTittle = findViewById(R.id.tvTittle);
         tvPera1 = findViewById(R.id.tvPera1);
-        tvUserName = findViewById(R.id.tvUserName);
+        tvEmail = findViewById(R.id.tvEmail);
         tvPassword = findViewById(R.id.tvPassword);
-        etUserName = findViewById(R.id.etUserName);
+        etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
         btnLoginPage = findViewById(R.id.btnLoginPage);
+        progressBar = findViewById(R.id.progressBar);
+
+        authProfile = FirebaseAuth.getInstance();
+
         
 
         //Go to signup page
@@ -49,15 +65,32 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
         //go to OTP page
-        btnLoginPage.setOnClickListener(new View.OnClickListener() {
+        btnLoginPage.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                String textEmail = etEmail.getText().toString();
+                String textPass = etPassword.getText().toString();
 
-                Intent i4 = new Intent(LoginActivity.this, phoneNumVerifyActivity.class);
-                startActivity(i4);
+                if (TextUtils.isEmpty(textEmail)) {
+                    Toast.makeText(LoginActivity.this, "Please enter your email address", Toast.LENGTH_LONG).show();
+                    etEmail.setError("Email is required");
+                    etEmail.requestFocus();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()) {
+                    Toast.makeText(LoginActivity.this, "Please re-enter your email address", Toast.LENGTH_LONG).show();
+                    etEmail.setError("Valid email is required");
+                    etEmail.requestFocus();
+                } else if (TextUtils.isEmpty(textPass)) {
+                    Toast.makeText(LoginActivity.this, "Please enter your password", Toast.LENGTH_LONG).show();
+                    etPassword.setError("Password is required");
+                    etPassword.requestFocus();
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(textEmail,textPass);
+                }
 
             }
         });
+
         //change into Signup page
         btnToggleL.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +100,22 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-
     //All Methode
+    private void loginUser(String email, String password) {
+        authProfile.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this,"You are loged in now",Toast.LENGTH_SHORT).show();
+                    Intent i4 = new Intent(LoginActivity.this, phoneNumVerifyActivity.class);
+                    startActivity(i4);
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"Enter a valid Email!",Toast.LENGTH_SHORT).show();
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+    
 }
